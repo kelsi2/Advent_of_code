803aad05e89ebd50f5a104b051408c7774f9fc70
root <root@DESKTOP-BEUDVQH.localdomain>
2020-12-03 17:33:52 -0800

    Complete day 2 part 2

--- day2.js
+++ day2.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const data = fs.readFileSync('./day2_data.txt', 'utf-8').split('\n');
 let validPassCount = 0;
 
+// PART 1
+
 for (const item of data) {
   let [range, letter, password] = item.split(" ");
   letter = letter.replace(/.$/, "");
@@ -13,6 +15,21 @@ for (const item of data) {
   const checkLength = new RegExp(`^.{${range.replace("-", ",")}}$`, "g");
 
   if (password.match(checkLength)) {
+    validPassCount++;
+  }
+}
+
+console.log(validPassCount);
+
+// PART 2
+
+for (const item of data) {
+  let [range, letter, password] = item.split(" ");
+  letter = letter.replace(/.$/, "");
+
+  [start, end] = range.split("-");
+
+  if ((password[start - 1] === letter) ^ (password[end - 1] === letter)) {
     validPassCount++;
   }
 }
